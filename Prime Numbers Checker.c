403f7477fc414706da357cba38ebb7a4e26185a3
Tarif Hasan Samin <samin990bd@gmail.com>
2024-10-26 12:12:26 +0600

    Add files via upload

---
+++ Prime Numbers Checker.c
@@ -0,0 +1,36 @@
+//Print prime numbers 1 to 100
+// User input 1 to 100
+
+#include <stdio.h>
+#include <ctype.h>
+int main() 
+{
+   int number, count=0,n,m;
+   printf("===Welcome to Prime numbers Checker by Samincode01===\n ");
+   printf("Enter starting: ");
+   scanf("%d",&m);
+   printf("Enter Last number: ");
+   scanf("%d",&n);
+   printf("%d to %d Prime numbers are:\n", m,n);
+  for(number=m;number<=n;number++)
+  {
+    
+    count=0;
+    if(number<=1) 
+    {
+    count=1;
+    }
+    for(int i=2;i<number;i++)
+        {   
+        if(number%i==0)
+        {
+            count=1;
+            break;
+        }
+        }
+    if(count==0)
+        printf("%d\n", number);
+    }
+   
+    return 0;
+}
