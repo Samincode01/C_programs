5455a6ff08903756bd8e79970beb2f96ea132e98
Tarif Hasan Samin <samin990bd@gmail.com>
2024-11-09 20:27:46 +0600

    Create Pattern-12(Stars"*").c

---
+++ Pattern-12(Stars"*").c
@@ -0,0 +1,29 @@
+/*
+*   *
+ * * 
+  *  
+ * * 
+*   *
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+int n, col, row;
+    printf("Input N= ");
+    scanf("%d",&n);
+    
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=n;col++)
+    {
+       if(row==col || row+col==n+1)
+            printf("*");
+       else
+        printf(" ");
+    }
+    printf("\n");
+    }
+    return 0;
+}
