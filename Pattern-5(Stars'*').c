26a31b6cb983949b84f8677207b60c596e00ac95
Tarif Hasan Samin <samin990bd@gmail.com>
2024-11-09 12:43:11 +0600

    Create Pattern-5(Stars'*').c

---
+++ Pattern-5(Stars'*').c
@@ -0,0 +1,28 @@
+/*  *
+   ***
+  *****
+ *******
+********* */
+#include <stdio.h>
+
+int main() 
+{
+int n, col, row;
+    printf("Input N= ");
+    scanf("%d",&n);
+    
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=n-row;col++)
+            { 
+                printf(" ");
+            }
+        for(col=1;col<=2*row-1;col++)
+        { 
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
