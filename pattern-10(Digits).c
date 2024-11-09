0b454d2c5b3eae209de3cf3f3c5749051093e528
Tarif Hasan Samin <samin990bd@gmail.com>
2024-11-09 19:33:53 +0600

    Create pattern-10(Digits).c

---
+++ pattern-10(Digits).c
@@ -0,0 +1,44 @@
+/*
+    1 
+   1 2 
+  1 2 3 
+ 1 2 3 4 
+1 2 3 4 5 
+
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+int n, col, row;
+    printf("Input N= ");
+    scanf("%d",&n);
+    
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=n-row;col++)
+            { 
+                printf(" ");
+            }
+        for(col=1;col<=row;col++)
+        { 
+            printf("%d ",col);
+        }
+        printf("\n");
+    }
+    for(row=n-1;row>=1;row--)
+    {
+        for(col=1;col<=n-row;col++)
+            { 
+                printf(" ");
+            }
+        for(col=1;col<=row;col++)
+        { 
+            printf("%d ",col);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
