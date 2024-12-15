b32a557b5c6cff183e6bb7d234fdeb780f5a6c86
Tarif Hasan Samin <samin990bd@gmail.com>
2024-12-15 21:58:31 +0600

    Update Array_Structure_Input.c

--- Structures/Array_Structure_Input.c
+++ Structures/Array_Structure_Input.c
@@ -1,7 +1,10 @@
-//Basic Structure
+//Array Structure
+//Input
 #include <stdio.h>
+#include <string.h> 
 struct person
 {
+        char name[50];
         int age;
         float salary;
 };
@@ -10,22 +13,40 @@ int main()
     int totalnum;
     printf("Enter Number of total person:\n");
     scanf("%d", &totalnum);
+    getchar();
     struct person person[totalnum];
     int i;
     for (i=0; i<totalnum; i++)
     {
-        printf("Enter Information for person %d:\n", i+1);
+        printf("Enter Information for person %d:\n", i + 1);
+
+        // Read name using fgets (safe method)
+        printf("Enter Name:\n");
+        fgets(person[i].name, sizeof(person[i].name), stdin);
+        
+        // Remove trailing newline from fgets input
+        person[i].name[strcspn(person[i].name, "\n")] = 0;
+
+        // Read age and salary
         printf("Enter Age:\n");
         scanf("%d", &person[i].age);
+        
+        // Consume the leftover newline from scanf
+        getchar();
+
         printf("Enter Salary:\n");
         scanf("%f", &person[i].salary);
+        
+        // Consume the leftover newline from scanf
+        getchar();
         
     }
     for (i=0; i<totalnum; i++)
     {
-        printf("Information for person %d:\n", i+1);
+        printf("Information for person %d:\n", i + 1);
+        printf("Name = %s\n", person[i].name);  // Now printing the name
         printf("Age = %d\n", person[i].age);
-        printf("Salary = %f\n", person[i].salary);
+        printf("Salary = %.2f\n", person[i].salary);
         
     }
   
