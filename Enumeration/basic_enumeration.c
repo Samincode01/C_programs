24421ab5cf1d090d4ee275e97d05c2bc7c1bd8fe
Tarif Hasan Samin <samin990bd@gmail.com>
2024-12-17 11:45:29 +0600

    Create basic_enumeration.c

---
+++ Enumeration/basic_enumeration.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+enum days_of_week
+{
+    Sun, Mon, Tue, Wed, Thu, Fri, Sat
+};
+int main() {
+    enum days_of_week day1, day2;
+    day1= Tue;
+    day2= Wed;
+    int dif = day2-day1;
+    printf(" day difference = %d", dif);
+    return 0;
+}
