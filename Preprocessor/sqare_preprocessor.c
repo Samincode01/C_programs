169249504a8e0eb3514537f5bc7dfaa52b3ffcd9
Tarif Hasan Samin <samin990bd@gmail.com>
2024-12-19 20:38:12 +0600

    Create sqare_preprocessor.c

---
+++ Preprocessor/sqare_preprocessor.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define SQUARE(x) ((x) * (x))   // Macro to calculate square of a number
+//You can also make custom header in C project to make easier
+
+int main() {
+    int num = 5;
+    printf("Square of %d is: %d\n", num, SQUARE(num));  // Will be replaced as ((5) * (5))
+    return 0;
+}
